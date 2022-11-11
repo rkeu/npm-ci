7ed88f965d82bc11a27f7740e51084828335d06b
Robin Knipe <robin.knipe@gmail.com>
2022-11-11 20:14:41 +0000

    see what github gives us

--- script.sh
+++ script.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-command="$1"
-who="$2"
-
-if [[ $command == "greet" ]]; then
-  echo "hello, $who"
-elif [[ $command == "error" ]]; then
-  echo "ERROR: script.sh, line 11: this is not so good"
-  exit 1
-fi
+env
